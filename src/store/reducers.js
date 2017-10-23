78b60e46937c517daa019ed868b89c0b2cc6fc3a
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-10-23 12:34:22 +0300

    Add day reducer added

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,11 @@
 import C from '../constants';
 
-export const goal = (state = 10, action) => {
-  if (action.type === C.SET_GOAL) {
-    return action.payload;
-  } else {
-    return state;
-  }
-};
+export const goal = (state = 10, action) =>
+  (action.type === C.SET_GOAL) ?
+    action.payload :
+    state;
+
+export const skiDay = (state = null, action) =>
+  (action.type === C.ADD_DAY) ?
+    action.payload :
+    state;
