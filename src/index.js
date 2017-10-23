9ba8423251ebcb0de6fe4eadbd0e3c8a0b4a8157
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-10-23 12:06:46 +0300

    Linter and configs added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Constants from './constants';
+import { allSkiDays, goal } from './initialState.json';
+
+console.log(`
+
+   Ski Day Counter
+   ================
+   The goal is ${goal} days
+   Initially there are ${allSkiDays.length} ski days in state
+
+   Constants (actions)
+   -------------------
+   ${Object.keys(Constants).join('\n     ')}
+
+`);
